f32ab001492cce98305c2f49e2c6174df9d38504
Steve Tu <stevetu11@yahoo.com>
2023-01-03 17:45:04 -0800

    Fix AccessibilityService not waiting for the actual gesture to finish

--- android/app/src/main/java/com/example/cv_bot_template/utils/MyAccessibilityService.kt
+++ android/app/src/main/java/com/example/cv_bot_template/utils/MyAccessibilityService.kt
@@ -14,6 +14,7 @@ import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 import android.widget.Toast
 import com.example.cv_bot_template.MainActivity.loggerTag
+import com.example.cv_bot_template.R
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
@@ -24,6 +25,7 @@ import kotlinx.coroutines.runBlocking
  */
 class MyAccessibilityService : AccessibilityService() {
 	private val tag: String = "${loggerTag}MyAccessibilityService"
+	private var appName: String = myContext.getString(R.string.app_name)
 	private lateinit var myContext: Context
 
 	companion object {
@@ -61,8 +63,8 @@ class MyAccessibilityService : AccessibilityService() {
 		instance = this
 		myContext = this
 
-		Log.d(tag, "Accessibility Service is now running.")
-		Toast.makeText(myContext, "Accessibility Service is now running.", Toast.LENGTH_SHORT).show()
+		Log.d(tag, "Accessibility Service for $appName is now running.")
+		Toast.makeText(myContext, "Accessibility Service for $appName is now running.", Toast.LENGTH_SHORT).show()
 	}
 
 	override fun onAccessibilityEvent(event: AccessibilityEvent?) {
@@ -76,8 +78,8 @@ class MyAccessibilityService : AccessibilityService() {
 	override fun onDestroy() {
 		super.onDestroy()
 
-		Log.d(tag, "Accessibility Service is now stopped.")
-		Toast.makeText(myContext, "Accessibility Service is now stopped.", Toast.LENGTH_SHORT).show()
+		Log.d(tag, "Accessibility Service for $appName is now stopped.")
+		Toast.makeText(myContext, "Accessibility Service for $appName is now stopped.", Toast.LENGTH_SHORT).show()
 	}
 
 	/**
@@ -140,12 +142,11 @@ class MyAccessibilityService : AccessibilityService() {
 	 * @param x The x coordinate of the point.
 	 * @param y The y coordinate of the point.
 	 * @param buttonName The name of the image to tap.
-	 * @param ignoreWait Whether or not to not wait 0.5 seconds after dispatching the gesture.
-	 * @param longPress Whether or not to long press.
-	 * @param taps How many taps to execute.
+	 * @param longPress Whether or not to long press. Defaults to false.
+	 * @param taps How many taps to execute. Defaults to a single tap.
 	 * @return True if the tap gesture was executed successfully. False otherwise.
 	 */
-	fun tap(x: Double, y: Double, buttonName: String, ignoreWait: Boolean = false, longPress: Boolean = false, taps: Int = 1): Boolean {
+	fun tap(x: Double, y: Double, buttonName: String, longPress: Boolean = false, taps: Int = 1): Boolean {
 		// Randomize the tapping location.
 		val (newX, newY) = randomizeTapLocation(x, y, buttonName)
 		Log.d(tag, "Tapping $newX, $newY for image: ${buttonName.uppercase()}")
@@ -175,16 +176,12 @@ class MyAccessibilityService : AccessibilityService() {
 
 		while (tries > 0) {
 			dispatchGesture(gesture, null, null)
-			if (!ignoreWait) {
-				0.5.wait()
-			}
+			0.25.wait()
 
 			tries -= 1
 		}
 
-		if (!ignoreWait) {
-			0.5.wait()
-		}
+		0.25.wait()
 
 		return dispatchResult
 	}
@@ -193,11 +190,10 @@ class MyAccessibilityService : AccessibilityService() {
 	 * Creates a scroll gesture either scrolling up or down the screen depending on the given action.
 	 *
 	 * @param scrollDown The scrolling action, either up or down the screen. Defaults to true which is scrolling down.
-	 * @param duration How long the scroll should take. Defaults to 100L.
-	 * @param ignoreWait Whether or not to not wait 0.5 seconds after dispatching the gesture.
+	 * @param duration How long the scroll should take. Defaults to 500L.
 	 * @return True if the scroll gesture was executed successfully. False otherwise.
 	 */
-	fun scroll(scrollDown: Boolean = true, duration: Long = 500L, ignoreWait: Boolean = false): Boolean {
+	fun scroll(scrollDown: Boolean = true, duration: Long = 500L): Boolean {
 		val scrollPath = Path()
 
 		// Get certain portions of the screen's dimensions.
@@ -244,9 +240,7 @@ class MyAccessibilityService : AccessibilityService() {
 		}.build()
 
 		val dispatchResult = dispatchGesture(gesture, null, null)
-		if (!ignoreWait) {
-			0.5.wait()
-		}
+		(duration.toDouble() / 1000).wait()
 
 		if (!dispatchResult) {
 			Log.e(tag, "Failed to dispatch scroll gesture.")
@@ -270,10 +264,9 @@ class MyAccessibilityService : AccessibilityService() {
 	 * @param newX The x coordinate of the new position.
 	 * @param newY The y coordinate of the new position.
 	 * @param duration How long the swipe should take. Defaults to 500L.
-	 * @param ignoreWait Whether or not to not wait 0.5 seconds after dispatching the gesture.
 	 * @return True if the swipe gesture was executed successfully. False otherwise.
 	 */
-	fun swipe(oldX: Float, oldY: Float, newX: Float, newY: Float, duration: Long = 500L, ignoreWait: Boolean = false): Boolean {
+	fun swipe(oldX: Float, oldY: Float, newX: Float, newY: Float, duration: Long = 500L): Boolean {
 		// Set up the Path by swiping from the old position coordinates to the new position coordinates.
 		val swipePath = Path().apply {
 			moveTo(oldX, oldY)
@@ -285,9 +278,9 @@ class MyAccessibilityService : AccessibilityService() {
 		}.build()
 
 		val dispatchResult = dispatchGesture(gesture, null, null)
-		if (!ignoreWait) {
-			0.5.wait()
-		}
+
+		// Wait for the stroke gesture to actually complete.
+		(duration.toDouble() / 1000).wait()
 
 		return dispatchResult
 	}
